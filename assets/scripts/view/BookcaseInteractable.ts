781cc2d352f6166ba17e7a5dd7975630a646a26b
lqx-nancy <416088226@qq.com>
2026-03-20 01:06:55 +0800

    bedroom主场景脚本修改

--- assets/scripts/view/BookcaseInteractable.ts
+++ assets/scripts/view/BookcaseInteractable.ts
@@ -1,53 +1,36 @@
-import { _decorator, Component, director,Node } from "cc";
-const { ccclass,property } = _decorator;
+import { _decorator, Component, director, Node, EventTouch } from "cc";
+const { ccclass, property } = _decorator;
+import { TwoBooksPopController } from "./TwoBooksPopController";
 
-const event = {
-    INTERACTABLE_TRIGGERED: "INTERACTABLE_TRIGGERED",
-    INTERACTABLE_CLICK: "INTERACTABLE_CLICK",
-} as const;
 
 @ccclass("BookcaseInteractable")
 export class BookcaseInteractable extends Component {
-    @property(Node)
-    public bookcaseClose: Node|null = null;
 
+    @property(TwoBooksPopController)
+    public popupController: TwoBooksPopController | null = null;
     private readonly interactableId = "point_bookcase";
 
+
     onEnable() {
-        director.on(event.INTERACTABLE_TRIGGERED, this.onTriggered, this);
-        this.node.on(Node.EventType.TOUCH_END,this.onclick,this)
+        //director.on(event.INTERACTABLE_TRIGGERED, this.onTriggered, this);
+        this.node.on(Node.EventType.TOUCH_END, this.onclick, this)
         console.log("BookcaseInteractable onEnable -> 注册监听 点击节点");
     }
 
     onDisable() {
-        director.off(event.INTERACTABLE_TRIGGERED, this.onTriggered, this);
-        this.node.off(Node.EventType.TOUCH_END,this.onclick,this)
+        //director.off(event.INTERACTABLE_TRIGGERED, this.onTriggered, this);
+        this.node.off(Node.EventType.TOUCH_END, this.onclick, this)
         console.log("BookcaseInteractable onDisable -> 移除监听");
     }
-    private onclick(){
-        console.log(`BookcaseInteractable onclick -> 点击节点,emit INTERACTABLE_CLICK: ${this.interactableId}`);
-       // director.emit(event.INTERACTABLE_CLICK, { interactableId: this.interactableId });
-        if(this.bookcaseClose){
-            this.bookcaseClose.active = true;
-        }
-    }
-
-    private onTriggered(result: any) {
-        console.log("[BookcaseInteractable] 收到交互事件", result);
-
-        if (result?.interactableId !== this.interactableId) {
-            console.log(`[BookcaseInteractable] 交互点不匹配: current=${result?.interactableId}, target=${this.interactableId}`);
+    private onclick() {
+        // director.emit(event.INTERACTABLE_CLICK, { interactableId: this.interactableId });
+        if (!this.popupController) {
+            console.error("[BookcaseInteractable] popupController 没有绑定");
             return;
         }
-
-        switch (result?.code) {
-            case "ENTER_BOOKCASE":
-                console.log("[BookcaseInteractable] 触发 ENTER_BOOKCASE -> 进入书柜特写");
-                return;
-
-            case "OPEN_TWOBOOKS_IN_BOOKCASE":
-                console.log("[BookcaseInteractable] 触发 OPEN_TWOBOOKS_IN_BOOKCASE -> 打开双书选项");
-                return;
-        }
+        this.popupController.openOptions();
+        console.log(`BookcaseInteractable onclick -> 点击节点: ${this.interactableId}`)
     }
+
+    
 }
--- assets/scripts/view/KeyInteractable.ts
+++ assets/scripts/view/KeyInteractable.ts
@@ -1,21 +1,23 @@
 import { _decorator, Component, director,Node } from "cc";
 const { ccclass, property } = _decorator;
-//import {dataManager} from "../manager/manager";
-// const event = {
-//     INTERACTABLE_TRIGGERED: "INTERACTABLE_TRIGGERED",
-//     INTERACTABLE_CLICK: "INTERACTABLE_CLICK",
-// }as const
+import {DataManager} from "../core/DataManager"
+
 @ccclass('KeyInteractable')
 export class KeyInteractable extends Component {
-    private readonly itemId = "point_key"
+    private readonly itemId = "key"
+    private readonly flagId = "PICK_KEY"
+
+    onLoad () {
+        if(DataManager.instance.getBool(this.flagId)){
+            this.node.active = false
+        }
+    }
 
     onEnable() {
-        //director.on(event.INTERACTABLE_TRIGGERED, this.onTriggered, this)
         this.node.on(Node.EventType.TOUCH_END,this.onClick,this)
                 console.log("KeyInteractable onEnable -> 注册监听,点击监听")
     }
     onDisable() {
-        //director.off(event.INTERACTABLE_TRIGGERED, this.onTriggered, this)
         this.node.off(Node.EventType.TOUCH_END,this.onClick,this)
                 console.log("KeyInteractable onDisable -> 取消监听，点击监听")
     }
@@ -24,18 +26,6 @@ export class KeyInteractable extends Component {
         console.log(`[KeyInteractable] 收到点击事件emit INTERACTABLE_CLICK: ${this.itemId}`)
         director.emit("ADD_ITEM_REQUEST", { itemId: this.itemId })
         this.node.active = false
-        //director.emit(event.INTERACTABLE_CLICK, this.interactableId)
     }
-    // private onTriggered(result: any) {
-    //     console.log("[KeyInteractable] 收到交互事件", result);
-    //     if (result.id !== this.interactableId){
-    //         console.log(`[KeyInteractable] 交互点不匹配: current=${result?.interactableId}, target=${this.interactableId}`);
-    //         return}
-    //     switch (result?.code) {
-    //         case "PICK_KEY":
-    //             console.log("[KeyInteractable] 已完成弹窗")
-    //             return
-    //     }
-
-    // }
+   
 }
