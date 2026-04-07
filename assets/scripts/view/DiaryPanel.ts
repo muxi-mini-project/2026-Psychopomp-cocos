c0ee32bc327db5b93b6dd3ab6ca4ea34c94281c2
lqx-nancy <416088226@qq.com>
2026-04-08 01:33:29 +0800

    日记密码面板、手机内软件跳转

---
+++ assets/scripts/view/DiaryPanel.ts
@@ -0,0 +1,38 @@
+import { Component,_decorator,Node } from "cc";
+const { ccclass, property } = _decorator;
+
+@ccclass('DiaryPanel')
+export class DiaryPanel extends Component {
+    @property({type:Node,tooltip:"日记密码面板"})
+    public diaryPswPanel:Node | null = null;
+
+    protected onEnable(): void {
+        this.node.on(Node.EventType.TOUCH_END, this.onClickOpen, this);
+    }
+
+    protected onDisable(): void {
+        this.node.off(Node.EventType.TOUCH_END, this.onClickOpen, this);
+    }
+
+    protected start(): void {
+        this.hidePanel();
+    }
+
+    protected openPanel():void{
+        console.log("打开日记密码面板");
+        if(this.diaryPswPanel){
+            this.diaryPswPanel.active = true;
+        }   
+    }
+
+    protected hidePanel():void{
+        if(this.diaryPswPanel){
+            this.diaryPswPanel.active = false;
+        }
+    }
+
+    protected onClickOpen():void{
+        this.openPanel();
+    }
+
+}
