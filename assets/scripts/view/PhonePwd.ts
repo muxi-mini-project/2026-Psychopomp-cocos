f55ca95c9aceeebda7bf1fbdc620da6d515111ef
ashes1452 <16416236+ashes1452@user.noreply.gitee.com>
2026-04-10 11:52:04 +0800

    手机密码更新

--- assets/scripts/view/PhonePwd.ts
+++ assets/scripts/view/PhonePwd.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Label, Button, tween, Vec3, director, Node, input } from 'cc'
+import { _decorator, Component, Label, Button, tween, Vec3, director, Node } from 'cc'
 const { ccclass, property } = _decorator
 
 @ccclass('PhonePwd')
@@ -8,19 +8,38 @@ export class PhonePwd extends Component {
     inputDisplayLabel: Label = null
 
     @property(Button)
-    delButton: Button = null
-
-    @property(Button)
     okButton: Button = null
 
     @property(Button)
-    clearButton: Button = null
-
-    @property(Button)
-    backButton: Button = null
-
-    @property(Button)
     enterButton: Button = null
+
+    @property(Button)
+    Btn_1: Button = null
+
+    @property(Button)
+    Btn_2: Button = null
+    @property(Button)
+    Btn_3: Button = null
+    @property(Button)
+    Btn_4: Button = null
+    @property(Button)
+    Btn_5: Button = null
+    @property(Button)
+    Btn_6: Button = null
+    @property(Button)
+    Btn_7: Button = null
+    @property(Button)
+    Btn_8: Button = null
+    @property(Button)
+    Btn_9: Button = null
+    @property(Button)
+    Btn_0: Button = null
+    @property(Button)
+    Btn_Clear: Button = null
+    @property(Button)
+    Btn_Del: Button = null
+    @property(Button)
+    Btn_Back: Button = null
 
     private readonly correctPwd = "W1S21T14"
     private inputStr = ""
@@ -40,36 +59,56 @@ export class PhonePwd extends Component {
 
     private lastKey = ""
     private currentIndex = 0
+    private isChoosing = false
 
     onLoad() {
-        this.bindAllButtons()
+        this.bindButtons()
         this.updateDisplay()
-        this.okButton.node.active = false
-        this.enterButton.node.active = true
+        this.showEnter() // 初始显示Enter
     }
 
+    bindButtons() {
+        this.Btn_1.node.on('click', () => this.onNumberClick("Btn_1", this.Btn_1.node), this)
+        this.Btn_2.node.on('click', () => this.onNumberClick("Btn_2", this.Btn_2.node), this)
+        this.Btn_3.node.on('click', () => this.onNumberClick("Btn_3", this.Btn_3.node), this)
+        this.Btn_4.node.on('click', () => this.onNumberClick("Btn_4", this.Btn_4.node), this)
+        this.Btn_5.node.on('click', () => this.onNumberClick("Btn_5", this.Btn_5.node), this)
+        this.Btn_6.node.on('click', () => this.onNumberClick("Btn_6", this.Btn_6.node), this)
+        this.Btn_7.node.on('click', () => this.onNumberClick("Btn_7", this.Btn_7.node), this)
+        this.Btn_8.node.on('click', () => this.onNumberClick("Btn_8", this.Btn_8.node), this)
+        this.Btn_9.node.on('click', () => this.onNumberClick("Btn_9", this.Btn_9.node), this)
+        this.Btn_0.node.on('click', () => this.onNumberClick("Btn_0", this.Btn_0.node), this)
 
-    bindAllButtons() {
-        const buttons = this.node.getComponentsInChildren(Button)
-        for (let btn of buttons) {
-            btn.node.on('click', () => {
-                this.onButtonClick(btn.node.name, btn.node)
-            })
-        }
+        this.Btn_Clear.node.on('click', () => this.onNumberClick("Btn_Clear", this.Btn_Clear.node), this)
+        this.Btn_Del.node.on('click', () => this.onNumberClick("Btn_Del", this.Btn_Del.node), this)
+        this.Btn_Back.node.on('click', () => this.onNumberClick("Btn_Back", this.Btn_Back.node), this)
+
+        this.okButton.node.on('click', () => this.onOk(), this)
+        this.enterButton.node.on('click', () => this.onEnter(), this)
     }
 
     playAnimation(target: Node) {
-        //这里可以根据需要添加按键动画
         tween(target)
             .to(0.1, { scale: new Vec3(0.9, 0.9, 0.9) })
             .to(0.1, { scale: new Vec3(1, 1, 1) })
             .start()
-
     }
 
-    onButtonClick(name: string, target: Node) {
+    onNumberClick(name: string, target: Node) {
+        if (!target.active) return
         director.emit("CLEAR_FAIL")
         this.playAnimation(target)
+
+        if (this.isChoosing) {
+            // 正在选字符 → 只允许：当前键 或 OK
+            if (name === this.lastKey || name === "Btn_Ok" || name === "Btn_Del" || name === "Btn_Clear") {
+                // 允许
+            } else {
+                // 其他键全部拦截！
+                return
+            }
+        }
+
         if (name === "Btn_Clear") {
             this.clearInput()
             return
@@ -77,7 +116,6 @@ export class PhonePwd extends Component {
 
         if (name === "Btn_Back") {
             director.emit("BACK")
-            //返回上一个界面
             return
         }
 
@@ -96,17 +134,24 @@ export class PhonePwd extends Component {
             return
         }
 
+
+
         if (!this.keyMap[name]) return
         const chars = this.keyMap[name]
 
+        this.showOk()
+
         if (this.lastKey === name) {
-            this.okButton.node.active = true
-            this.enterButton.node.active = false
             this.inputStr = this.inputStr.slice(0, -1)
             this.currentIndex = (this.currentIndex + 1) % chars.length
-        }
-        else {
+        } else {
             this.currentIndex = 0
+        }
+
+        if (chars.length > 1) {
+            this.showOk()
+        } else {
+            this.showEnter()
         }
 
         const c = chars[this.currentIndex]
@@ -120,14 +165,15 @@ export class PhonePwd extends Component {
         this.updateDisplay()
     }
 
+
     onOk() {
         this.lastKey = ""
         this.currentIndex = 0
-        this.okButton.node.active = false
-        this.enterButton.node.active = true
+        this.showEnter()
     }
 
     onEnter() {
+        this.playAnimation(this.enterButton.node)
         this.checkPassword()
     }
 
@@ -135,10 +181,7 @@ export class PhonePwd extends Component {
         if (this.inputStr.toUpperCase() === this.correctPwd) {
             console.log("密码正确")
             director.emit("PHONE_PWD_SUCCESS")
-            //密码正确
-            //触发后续
         } else {
-            //密码错误，震动并清空输入
             console.log("密码错误")
             director.emit("PHONE_PWD_FAIL")
             setTimeout(() => this.clearInput(), 500)
@@ -150,6 +193,9 @@ export class PhonePwd extends Component {
         if (this.inputStr.length > 0) {
             this.inputStr = this.inputStr.slice(0, -1)
             this.updateDisplay()
+            this.showEnter()
+            this.lastKey = ""
+            this.currentIndex = 0
         }
     }
 
@@ -157,15 +203,26 @@ export class PhonePwd extends Component {
         this.inputStr = ""
         this.lastKey = ""
         this.currentIndex = 0
+        this.showEnter()
         this.updateDisplay()
     }
 
-    //同步到输入框
     updateDisplay() {
         this.inputDisplayLabel.string = this.inputStr
     }
 
-    //震动
+    showOk() {
+        this.isChoosing = true
+        this.okButton.node.active = true
+        this.enterButton.node.active = false
+    }
+
+    showEnter() {
+        this.isChoosing = false
+        this.okButton.node.active = false
+        this.enterButton.node.active = true
+    }
+
     shake() {
         tween(this.node)
             .by(0.05, { x: -6 })
